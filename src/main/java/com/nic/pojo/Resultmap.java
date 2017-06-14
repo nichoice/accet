101a821b52f5aa451e89d88a51268b2e87e4b485
nichoice <nichoice@outlook.com>
2017-06-14 15:21:13 +0800

    修改语句跟返回集

--- src/main/java/com/nic/pojo/Resultmap.java
+++ src/main/java/com/nic/pojo/Resultmap.java
@@ -12,20 +12,52 @@ import java.util.Date;
  */
 @ExcelTarget("event_list")
 public class Resultmap implements Serializable {
-    private static final long serialVersionUID = -721663237729842431L;
+    private static final long serialVersionUID = 3346470836595972999L;
+    @Excel(name = "设备名称")
+    private String d_name;
     @Excel(name = "IP地址")
-    private String address;
+    private String d_address;
+    @Excel(name = "故障")
+    private String t_name;
+    @Excel(name = "code")
+    private String c_name;
     @Excel(name = "故障时间" , format = "yyyy-MM-dd hh:mm:ss")
     private Date time;
-    @Excel(name = "设备名称")
-    private String name;
 
-    public String getAddress() {
-        return address;
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
     }
 
-    public void setAddress(String address) {
-        this.address = address;
+    public String getD_name() {
+        return d_name;
+    }
+
+    public void setD_name(String d_name) {
+        this.d_name = d_name;
+    }
+
+    public String getD_address() {
+        return d_address;
+    }
+
+    public void setD_address(String d_address) {
+        this.d_address = d_address;
+    }
+
+    public String getT_name() {
+        return t_name;
+    }
+
+    public void setT_name(String t_name) {
+        this.t_name = t_name;
+    }
+
+    public String getC_name() {
+        return c_name;
+    }
+
+    public void setC_name(String c_name) {
+        this.c_name = c_name;
     }
 
     public Date getTime() {
@@ -35,13 +67,4 @@ public class Resultmap implements Serializable {
     public void setTime(Date time) {
         this.time = time;
     }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
 }
